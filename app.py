a886772d550bd4b723187fd0185ca5744767062a
shigeokatakeshi <takeshi0713@gmail.com>
2022-07-04 23:35:36 +0900

    あやさんのお力によりtwitterのアカウント情報が取得可能な状況だが、アカウントの申請待ち

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+import tweepy
+from dotenv import load_dotenv
+
+# env読み込み
+load_dotenv()
+# クライアント変数を作成
+bearer_token = os.environ.get("BEARER_TOKEN")
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_KEY")
+access_token_secret = os.environ.get("ACCESS_SECRET_KEY")
+
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+
+def main():
+    screen_name = input()
+    user = api.get_user(screen_name=screen_name)
+    user_info = [
+        # user.id_str,
+        # user.screen_name,
+        # user.name,
+        user.followers_count,
+        # user.description,
+    ]
+    print(user_info)
+
+
+if __name__ == "__main__":
+    main()
---
+++ youtube.py
@@ -0,0 +1,34 @@
+import requests
+import time
+
+
+id = 0
+
+
+def main():
+    id = input("You TubeIDを入力して下さい:")
+    print(id)
+    response = requests.get(
+        "https://www.googleapis.com/youtube/v3/channels?part=statistics&id={id}&key=AIzaSyBDCx3PIGQQnXlJ_qSTtjAs6omJ1svWXVk"
+    )
+
+    print(response)
+
+    time.sleep(1)  # ここで1秒止まる
+
+    # dic = response.json()
+    # number_d = input("Hacker Newsトップのニュースをいくつ表示しますか？:")
+    # for top_page_news in range(0, int(number_d)):
+    #     # print(dic[newstopics])
+    #     news_id = dic[top_page_news]
+    #     contents = requests.get(
+    #         f"https://hacker-news.firebaseio.com/v0/item/{news_id}.json?print=pretty"
+    #     )
+    #     data = contents.json()
+    #     print("'title': '", data.get("title"), "', 'Link':", data.get("url", None))
+
+    #     time.sleep(1)  # ここで1秒止まる
+
+
+if __name__ == "__main__":
+    main()
